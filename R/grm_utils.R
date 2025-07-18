20ccee39517c03d8eaa71d9fbbd4468ed448f405
WyattGMadden <wyattgmadden@gmail.com>
2025-07-18 12:48:53 -0400

    previously error if just one column in L or M matrix

--- R/grm_utils.R
+++ R/grm_utils.R
@@ -4,9 +4,9 @@ covariate_matrix_standardize <- function(x) {
   x <- as.matrix(x)
   x.mean <- apply(x, 2, mean)
   x.sd <- apply(x, 2, stats::sd)
-  x[, x.sd != 0] <- sweep(sweep(x[, x.sd != 0], 
+  x[, x.sd != 0] <- sweep(sweep(x[, x.sd != 0, drop = F], 
                                 2, 
-                                x.mean[x.sd != 0], 
+                                x.mean[x.sd != 0, drop = F], 
                                 "-"),
                           2,
                           x.sd[x.sd != 0],
